7db44264d83d1df2d64ac296e7c0239b7fe9c0cb
devshahoriar <shuvo1556@gmail.com>
2025-07-25 12:42:40 +0600

    fix build error

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/consistent-type-imports */
 "use client"
 
 import { useTheme } from "next-themes"
-import { Toaster as Sonner, ToasterProps } from "sonner"
+import { Toaster as Sonner, type ToasterProps } from "sonner"
 
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme()
