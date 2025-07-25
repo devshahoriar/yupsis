3252e01bc12eed8205e83ee92202a3a9ea2d9b8b
devshahoriar <shuvo1556@gmail.com>
2025-07-25 21:33:21 +0600

    update fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import { FeaturedProducts } from '@/components/pages/home/featured-products';
 import { HeroSection } from "@/components/pages/home/hero-section";
+import { api } from '@/trpc/server';
 
 export default async function Home() {
+  void await api.product.getFeatured.prefetch();
   return (
     <div className="min-h-screen">
       <HeroSection />
